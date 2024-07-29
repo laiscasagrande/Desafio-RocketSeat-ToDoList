bb8df6b30a45310be60ac706025e216faf015577
Laís <lais.casagrande@agpr5.com>
2024-07-29 08:32:54 -0300

    :sparkles: feat: Botão excluir desabilitado após finalização da tarefa

--- src/components/TaskCompleted.tsx
+++ src/components/TaskCompleted.tsx
@@ -1,25 +1,29 @@
 import { Trash } from 'phosphor-react'
 import { PropsTask } from '../models/taskProps'
 import styles from './TaskCompleted.module.css'
+import { useState } from 'react'
 
 export function TaskCompleted({ description, onTaskDeleted, onTaskCompleted }: PropsTask) {
+
+    const [buttonDisabled, setButtonDisabled] = useState(false)
 
     function handleDeleteTask() {
         onTaskDeleted(description) //esse description é como se você o id da tarefa, estou passando como uma forma de identificação única para esxclusão daquela tarefa específica
     }
 
-function handleTaskCheck(){
-    onTaskCompleted()
-}
+    function handleTaskCheck() {
+        onTaskCompleted()
+        setButtonDisabled(true)
+    }
 
     return (
         <main className={styles.contentTask}>
             <label className={styles.description}>
-                <input type="radio" onClick={handleTaskCheck} id="task"/>
+                <input type="radio" onClick={handleTaskCheck} id="task" />
                 <span></span>
                 {description}
             </label>
-            <button className={styles.buttonTrash} onClick={handleDeleteTask}><Trash size={24} /></button>
+            <button className={styles.buttonTrash} onClick={handleDeleteTask} disabled={buttonDisabled}><Trash size={24} /></button>
         </main>
     )
 }
